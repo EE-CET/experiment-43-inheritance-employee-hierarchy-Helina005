0989a1ea800c06c3f173db821f82390a6c1345a8
Helina Alex <helinatalex@gmail.com>
2026-04-10 18:48:17 +0530

    error

--- src/EmployeeInheritance.java
+++ src/EmployeeInheritance.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-// Parent Class
 class Employee {
     String name;
     int age;
@@ -8,38 +7,35 @@ class Employee {
     String address;
     int salary;
 
-    // Method to print only the salary as requested
     void printSalary() {
         System.out.println(salary);
     }
 }
 
-// Subclass Officer inherits from Employee
 class Officer extends Employee {
     String specialization;
 
     void displayOfficer() {
-        System.out.println("\nOfficer:");
+        System.out.println("Officer:");
         System.out.println(name);
-        System.out.println( age);
-        System.out.println( phoneNumber);
-        System.out.println( address);
-        printSalary(); // Calling method from parent class
-        System.out.println( specialization);
+        System.out.println(age);
+        System.out.println(phoneNumber);
+        System.out.println(address);
+        printSalary();
+        System.out.println(specialization);
     }
 }
 
-// Subclass Manager inherits from Employee
 class Manager extends Employee {
     String department;
 
     void displayManager() {
         System.out.println("Manager:");
-        System.out.println( name);
-        System.out.println( age);
-        System.out.println( phoneNumber);
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(phoneNumber);
         System.out.println(address);
-        printSalary(); // Calling method from parent class
+        printSalary();
         System.out.println(department);
     }
 }
@@ -48,29 +44,29 @@ public class EmployeeInheritance {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Assigning values to Officer
+        // Officer Input
         Officer off = new Officer();
         off.name = sc.nextLine();
         off.age = sc.nextInt();
-        sc.nextLine(); // Consume newline
+        sc.nextLine(); 
         off.phoneNumber = sc.nextLine();
         off.address = sc.nextLine();
         off.salary = sc.nextInt();
-        sc.nextLine(); // Consume newline
+        sc.nextLine(); 
         off.specialization = sc.nextLine();
 
-        // Assigning values to Manager
+        // Manager Input
         Manager mng = new Manager();
         mng.name = sc.nextLine();
         mng.age = sc.nextInt();
-        sc.nextLine(); // Consume newline
+        sc.nextLine(); 
         mng.phoneNumber = sc.nextLine();
         mng.address = sc.nextLine();
-        mng.salary = mng.salary = sc.nextInt();
-        sc.nextLine(); // Consume newline
+        mng.salary = sc.nextInt();
+        sc.nextLine(); 
         mng.department = sc.nextLine();
 
-        // Printing Details
+        // Output
         off.displayOfficer();
         mng.displayManager();
         
